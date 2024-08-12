83436f791f95e31538a767a121d55aa010ed8a66
Bruno Rodrigues <bruno_151997@hotmail.com>
2024-08-12 20:50:22 -0300

    fix: back button

--- src/components/HeaderAction/styles.ts
+++ src/components/HeaderAction/styles.ts
@@ -6,11 +6,12 @@ export const Container = styled.View`
 export const Button = styled.TouchableOpacity`
   position: absolute;
   left: 0px;
+  z-index: 10;
 `
 export const Title = styled.Text`
   flex: 1;
   text-align: center;
   color: ${props => props.theme.COLORS.GRAY_1};
-  font-size: ${props => props.theme.FONT_SIZE.LG};
+  font-size: ${props => props.theme.FONT_SIZE.LG}px;
   font-family: ${props => props.theme.FONT_WEIGHT.BOLD};
 `
