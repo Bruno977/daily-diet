1117e231c42b74a16d489c893cba66586571e384
Bruno Rodrigues <bruno_151997@hotmail.com>
2024-08-13 20:03:09 -0300

    style: color header

--- src/components/HeaderPage/index.tsx
+++ src/components/HeaderPage/index.tsx
@@ -1,17 +1,26 @@
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import * as S from "./styles";
 import { HeaderAction } from "../HeaderAction";
+import { useTheme } from "styled-components/native";
 
-export function HeaderPage() {
+type HeaderPage = {
+  title: string;
+  color?: string;
+};
+
+export function HeaderPage({ title, color }: HeaderPage) {
   const insets = useSafeAreaInsets();
+  const theme = useTheme();
+  const backgroundColor = color || theme.COLORS.GRAY_5;
   return (
     <S.ContainerHeader
       style={{
         paddingLeft: insets.left + 24,
         paddingRight: insets.right + 24,
+        backgroundColor: backgroundColor,
       }}
     >
-      <HeaderAction title="Nova Refeição" />
+      <HeaderAction title={title} />
     </S.ContainerHeader>
   );
 }
--- src/components/HeaderPage/styles.ts
+++ src/components/HeaderPage/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components/native";
 
 export const ContainerHeader = styled.View`
   min-height: 132px;
-  background-color: ${props => props.theme.COLORS.GRAY_5};
   align-items: center;
   justify-content: center;
 `
