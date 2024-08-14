91405ab932199bf18c6962f3580941f9db74d12a
Bruno Rodrigues <bruno_151997@hotmail.com>
2024-08-14 19:22:31 -0300

    feat: add input Date New Meal

--- src/screens/NewMeal/index.tsx
+++ src/screens/NewMeal/index.tsx
@@ -7,11 +7,21 @@ import { useState } from "react";
 import { ButtonMealStatusProps } from "./types";
 import { useNavigation } from "@react-navigation/native";
 import { HeaderPage } from "../../components/HeaderPage";
+import { DatePicker } from "../../components/DatePicker";
+import { ModeProps } from "../../components/DatePicker/types";
+import { InputDate } from "../../components/InputDate";
+import { DateTimePickerEvent } from "@react-native-community/datetimepicker";
+
+import { formatDate, formatTime } from "../../utils/formatDate";
 
 export function NewMeal() {
   const { navigate } = useNavigation();
   const [buttonStatus, setButtonStatus] =
     useState<ButtonMealStatusProps>("inDiet");
+
+  const [date, setDate] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [mode, setMode] = useState<ModeProps>("date");
 
   function handlePressButtonMealStatus(status: ButtonMealStatusProps) {
     setButtonStatus(status);
@@ -20,16 +30,41 @@ export function NewMeal() {
   function handleCreateMeal() {
     navigate("RegisteredMeal", { mealStatus: buttonStatus });
   }
+
+  function handleBlurDateType(mode: ModeProps) {
+    setMode(mode);
+    setShowDatePicker(!showDatePicker);
+  }
+
+  function handleChangeDate(event: DateTimePickerEvent, date?: Date) {
+    setShowDatePicker(!showDatePicker);
+    setDate(date || new Date());
+  }
+
   return (
     <>
+      <DatePicker
+        visible={showDatePicker}
+        mode={mode}
+        date={date}
+        onChange={handleChangeDate}
+      />
       <HeaderPage title="Nova Refeição" />
       <S.Container>
         <S.GridContainer>
           <Input label="Nome" />
           <Input label="Descrição" isTextArea />
           <S.GridRow>
-            <Input label="Data" fullWidth />
-            <Input label="Hora" fullWidth />
+            <InputDate
+              label="Data"
+              value={formatDate(date)}
+              onPress={() => handleBlurDateType("date")}
+            />
+            <InputDate
+              label="Hora"
+              value={formatTime(date)}
+              onPress={() => handleBlurDateType("time")}
+            />
           </S.GridRow>
           <View style={{ flex: 1, gap: 8 }}>
             <S.Subtitle>Está dentro da Dieta?</S.Subtitle>
