67400c9d9f928089d06d95c273b8694ccb436bea
Bruno Rodrigues <bruno_151997@hotmail.com>
2024-08-15 20:04:26 -0300

    feat: create util format date title

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -4,7 +4,12 @@ function formatDate(date: Date) {
   return format(date, "dd/MM/yyyy");
 }
 
+function formatDateTitle(date: Date) {
+  return format(date, "dd.MM.yyyy");
+}
+
 function formatTime(date: Date) {
   return format(date, "HH:mm");
 }
-export {formatDate, formatTime}
+
+export {formatDate, formatTime, formatDateTitle}
