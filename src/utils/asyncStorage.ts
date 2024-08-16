e03c7ecfcdea71d83cdad331bc8c85a56b9b1f92
Bruno Rodrigues <bruno_151997@hotmail.com>
2024-08-15 21:15:47 -0300

    feat: create util find meal

--- src/utils/asyncStorage.ts
+++ src/utils/asyncStorage.ts
@@ -1,12 +1,36 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { DataStorageProps } from "../@types/storage";
 
-export const DATA_MEAL_STORAGE_KEY = "@daily-diet:data"
-async function createStorage(key: string, value: string){
-  await AsyncStorage.setItem(key, value)
+export const DATA_MEAL_STORAGE_KEY = "@daily-diet:data";
+async function createStorage(key: string, value: string) {
+  await AsyncStorage.setItem(key, value);
 }
 
-async function getStorage(key: string){
-  const response = await AsyncStorage.getItem(key)
+async function getStorage(key: string) {
+  const response = await AsyncStorage.getItem(key);
   return response ? JSON.parse(response) : [];
 }
-export {createStorage, getStorage}
+
+async function getMealStorage(id: string) {
+  const response: DataStorageProps[] = await getStorage(DATA_MEAL_STORAGE_KEY);
+
+  if (!response || response.length === 0) {
+    return null;
+  }
+
+  for (const data of response) {
+    const foundItem = data.data.find((item) => item.id === id);
+    if (foundItem) {
+      return {
+        description: foundItem.description,
+        hour: foundItem.hour,
+        id: foundItem.id,
+        inDiet: foundItem.inDiet,
+        title: foundItem.title,
+      };
+    }
+  }
+
+  return null;
+}
+export { createStorage, getStorage, getMealStorage };
