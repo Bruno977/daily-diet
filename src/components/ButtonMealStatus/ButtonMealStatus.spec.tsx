e3afd72821a3dcbeb24aa6a320d7cee7fa069cdc
Bruno Rodrigues <bruno_151997@hotmail.com>
2024-08-21 15:01:17 -0300

    test: validade background button

--- src/components/ButtonMealStatus/ButtonMealStatus.spec.tsx
+++ src/components/ButtonMealStatus/ButtonMealStatus.spec.tsx
@@ -1,11 +1,23 @@
-import { ButtonMealStatus } from ".";
+import { BUTTON_MEAL_STATUS_TEST_ID, ButtonMealStatus } from ".";
 import { render } from "../../../__tests__/utils/customRender";
 
 describe("<ButtonMealStatus/>", () => {
   it("should render correctly", () => {
-    const { getByText } = render(
+    const { getByText, getByTestId } = render(
       <ButtonMealStatus title="sim" variant="primary" />
     );
     expect(getByText("sim")).toBeTruthy();
+    expect(getByTestId(BUTTON_MEAL_STATUS_TEST_ID)).toHaveStyle({
+      backgroundColor: "#EFF0F0",
+    });
+  });
+  it("should render with variant style", () => {
+    const { getByText, getByTestId } = render(
+      <ButtonMealStatus title="sim" variant="primary" isActive />
+    );
+    expect(getByText("sim")).toBeTruthy();
+    expect(getByTestId(BUTTON_MEAL_STATUS_TEST_ID)).toHaveStyle({
+      backgroundColor: "#E5F0DB",
+    });
   });
 });
--- src/components/ButtonMealStatus/index.tsx
+++ src/components/ButtonMealStatus/index.tsx
@@ -6,6 +6,7 @@ type ButtonMealStatusProps = PressableProps & {
   variant: "primary" | "danger";
   isActive?: boolean;
 };
+export const BUTTON_MEAL_STATUS_TEST_ID = "ButtonMealStatus";
 export function ButtonMealStatus({
   title,
   variant,
@@ -13,7 +14,12 @@ export function ButtonMealStatus({
   ...rest
 }: ButtonMealStatusProps) {
   return (
-    <S.ButtonContainer isActive={isActive} variant={variant} {...rest}>
+    <S.ButtonContainer
+      isActive={isActive}
+      variant={variant}
+      testID={BUTTON_MEAL_STATUS_TEST_ID}
+      {...rest}
+    >
       <S.Circle variant={variant} isActive={isActive} />
       <S.Label>{title}</S.Label>
     </S.ButtonContainer>
